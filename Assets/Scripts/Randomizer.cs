e07e969111b2823aa422d3119aaf048539d9becd
David Briscoe <idbrii@gmail.com>
2015-02-21 11:42:37 -0800

    Randomly spawn creatures
    Add a generic random spawner.
    
    Spawn creatures with random spawner.

--- Assets/Scripts/Randomizer.cs
+++ Assets/Scripts/Randomizer.cs
@@ -13,7 +13,7 @@ public class Randomizer {
         }
     }
 
-    public int Range(int min, int max) {
+    public float Range(float min, float max) {
         if (max == min)
         {
             return min;
@@ -22,6 +22,37 @@ public class Randomizer {
         const float RAND_MAX = 1.0f;
         float scale = RAND_MAX / (float)(max - min);
         float r = min + (Random.value / scale);
+        return r;
+    }
+
+    public int Range(int min, int max) {
+        float min_f = min;
+        float max_f = max;
+        float r = Range(min_f, max_f);
         return (int)Mathf.Round(r);
+    }
+
+    public float PositiveNumber(float max) {
+        const float zero = 0;
+        return Range(zero, max);
+    }
+
+    public int PositiveNumber(int max) {
+        const int zero = 0;
+        return Range(zero, max);
+    }
+
+    public Vector3 UnitVectorFlat() {
+        const float max = 5.0f;
+        Vector3 v = new Vector3(Range(-max,max), Range(-max,max));
+        v.Normalize();
+        return v;
+    }
+
+    public Vector3 UnitVector() {
+        const float max = 5.0f;
+        Vector3 v = new Vector3(Range(-max,max), Range(-max,max), Range(-max,max));
+        v.Normalize();
+        return v;
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    [Tooltip("The prefab to spawn.")]
+    public Transform typeToSpawn;
+    [Tooltip("How many to spawn.")]
+    public int numToSpawn = 5;
+    [Tooltip("How big of a circular area to spawn.")]
+    public float spawnRadius = 5.0f;
+    [Tooltip("Set this object as the parent of new objects.")]
+    public bool shouldParentNewObjectsToMe = true;
+    [Tooltip("Instead of random spacing, force to spawnRadius from Spawner.")]
+    public bool forceToMaxDistance = false;
+    
+    
+    
+    private Randomizer rand;
+    
+    
+    void Awake() {
+        rand = new Randomizer();
+    }
+
+
+	void Start() {
+        SpawnObjects();
+    }
+
+    void SpawnObjects() {
+        Quaternion rot = transform.rotation;
+
+        for (int i = 0; i < numToSpawn; ++i) {
+            Vector3 offset = GetSpawnOffset();
+            Vector3 pos = transform.position + offset;
+            Transform obj = (Transform) Instantiate(typeToSpawn, pos, rot);
+            if (shouldParentNewObjectsToMe) {
+                obj.parent = transform;
+            }
+        }
+	
+	}
+
+    float GetSpawnDistance() {
+        if (forceToMaxDistance) {
+            return spawnRadius;
+        }
+        else {
+            return rand.PositiveNumber(spawnRadius);
+        }
+    }
+
+    Vector3 GetSpawnOffset() {
+        Vector3 offset_direction = rand.UnitVectorFlat();
+        offset_direction *= GetSpawnDistance();
+        return offset_direction;
+    }
+}

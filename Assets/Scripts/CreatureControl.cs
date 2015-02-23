e75c360c9e777582c13b217291dbacc83432e577
David Briscoe <idbrii@gmail.com>
2015-02-22 19:45:25 -0800

    Add obstacle avoidance

--- Assets/Scripts/CreatureControl.cs
+++ Assets/Scripts/CreatureControl.cs
@@ -8,18 +8,17 @@ public class CreatureControl : Mob
 	public float movementReduceTime = 1;
 	
 	[Tooltip("How close is too close.")]
-	public float tooClose = 1;
+	public float obstacleTooClose = 1;
+	public float creatureTooClose = 1;
 
-	public float repulsionMagnitude = 1;
+	public float obstacleRepulsionMagnitude = 1;
+	public float creatureRepulsionMagnitude = 1;
 	public float cohesionMagnitude = 1;
 	public float alignmentMagnitude = 1;
 	public float maxMagnitude = 3;
-	
-	private Vector2 direction;
-    private Vector2 directionRate = Vector2.zero;
-    private float urgency = 0;
 
-	List<CreatureControl> neighbouringCreatures = new List<CreatureControl>();
+	List<Transform> neighbouringCreatures = new List<Transform>();
+	List<Transform> obstacles = new List<Transform>();
 
     void Awake()
     {
@@ -28,35 +27,45 @@ public class CreatureControl : Mob
 
     void Update()
     {
-		if (neighbouringCreatures.Count == 0)
+		Vector3 totalForce = Vector3.zero;
+
+		if (obstacles.Count > 0)
 		{
-			return;
+			Vector3 obstacleRepulsionForce = getRepulsion(obstacles, obstacleTooClose);
+			obstacleRepulsionForce *= obstacleRepulsionMagnitude;
+
+			totalForce += obstacleRepulsionForce;
+
+			Debug.DrawRay(transform.position, obstacleRepulsionForce, Color.yellow);
 		}
 
-		Vector3 repulsionForce = getRepulsion();
-		repulsionForce *= repulsionMagnitude;
+		if (neighbouringCreatures.Count > 0)
+		{
+			Vector3 creatureRepulsionForce = getRepulsion(neighbouringCreatures, creatureTooClose);
+			creatureRepulsionForce *= creatureRepulsionMagnitude;
 
-		Vector3 averagePosition;
-		Vector2 averageDirection;
-		getAverages(out averagePosition, out averageDirection);
+			Vector3 averagePosition;
+			Vector2 averageDirection;
+			getAverages(out averagePosition, out averageDirection);
 
-		Vector3 towardCenter = averagePosition - transform.position;
-		towardCenter.Normalize();
+			Vector3 towardCenter = averagePosition - transform.position;
+			towardCenter.Normalize();
 
-		Vector3 towardCenterForce = towardCenter * cohesionMagnitude;
+			Vector3 towardCenterForce = towardCenter * cohesionMagnitude;
 
-		Vector3 alignmentForce = (Vector3)averageDirection * alignmentMagnitude;
+			Vector3 alignmentForce = (Vector3)averageDirection * alignmentMagnitude;
 
-		Vector3 totalForce = repulsionForce + towardCenterForce + alignmentForce;
+			totalForce += creatureRepulsionForce + towardCenterForce + alignmentForce;
+
+			Debug.DrawRay(transform.position, creatureRepulsionForce, Color.red);
+			Debug.DrawRay(transform.position, towardCenterForce, Color.green);
+			Debug.DrawRay(transform.position, alignmentForce, Color.blue);
+		}
+
 		float magnitude = Mathf.Min(maxMagnitude, totalForce.magnitude);
 		totalForce = totalForce.normalized * magnitude;
 
-		Debug.DrawRay(transform.position, repulsionForce, Color.red);
-		Debug.DrawRay(transform.position, towardCenterForce, Color.green);
-		Debug.DrawRay(transform.position, alignmentForce, Color.blue);
 		Debug.DrawRay(transform.position, totalForce);
-
-		//rigidbody2D.AddForce((Vector2)totalForce);
 
 		UpdateMovement(totalForce.x, totalForce.y);
     }
@@ -76,13 +85,14 @@ public class CreatureControl : Mob
 		averageDirection /= neighbouringCreatures.Count;
 	}
 
-	Vector3 getRepulsion()
+	Vector3 getRepulsion(List<Transform> repulsors, float tooClose)
 	{
 		Vector3 sumRepulsion = new Vector3();
 
-		foreach (var creature in neighbouringCreatures)
+		foreach (var repulsor in repulsors)
 		{
-			Vector3 awayVector = transform.position - creature.transform.position;
+			Vector3 awayVector = transform.position - repulsor.position;
+
 			if (awayVector.magnitude < tooClose)
 			{
 				var scale = (tooClose - awayVector.magnitude) / tooClose;
@@ -98,7 +108,11 @@ public class CreatureControl : Mob
         var creature = collider.gameObject.GetComponent<CreatureControl>();
 		if (creature != null)
         {
-			neighbouringCreatures.Add(creature);
+			neighbouringCreatures.Add(creature.transform);
+		}
+		else
+		{
+			obstacles.Add(collider.transform);
 		}
     }
 
@@ -107,7 +121,11 @@ public class CreatureControl : Mob
 		var creature = collider.gameObject.GetComponent<CreatureControl>();
 		if (creature != null)
 		{
-			neighbouringCreatures.Remove(creature);
+			neighbouringCreatures.Remove(creature.transform);
+		}
+		else
+		{
+			obstacles.Remove(collider.transform);
 		}
 	}
 }

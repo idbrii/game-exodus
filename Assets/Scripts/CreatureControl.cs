0b6fd50928e7a7bf52429ec5cbddd02eca8a1358
David Briscoe <idbrii@gmail.com>
2015-02-27 19:55:31 -0800

    Revert "Add obstacle avoidance"
    Instead of handling obstacles as another list, we're going to handle
    them as another kind of neighbour.
    
    This reverts commit e75c360c9e777582c13b217291dbacc83432e577.
    
    Conflicts:
    	Assets/Prefabs/Creature.prefab
    	Assets/Scripts/CreatureControl.cs
    	Assets/exodus.unity

--- Assets/Scripts/CreatureControl.cs
+++ Assets/Scripts/CreatureControl.cs
@@ -5,13 +5,10 @@ using System.Collections.Generic;
 public class CreatureControl : MonoBehaviour
 {
 	[Tooltip("How close is too close.")]
-	public float obstacleTooClose = 2;
-	public float creatureTooClose = 2;
+	public float tooClose = 2;
 
     [Range(1,250)]
-	public float obstacleRepulsionMagnitude = 50;
-    [Range(1,250)]
-	public float creatureRepulsionMagnitude = 10;
+	public float repulsionMagnitude = 10;
     [Range(1,250)]
 	public float cohesionMagnitude = 5;
     [Range(1,250)]
@@ -20,7 +17,6 @@ public class CreatureControl : MonoBehaviour
 	public float maxMagnitude = 50;
 
 	List<Transform> neighbouringCreatures = new List<Transform>();
-	List<Transform> obstacles = new List<Transform>();
 
 	Vector3 totalForce = Vector3.zero;
 
@@ -30,45 +26,33 @@ public class CreatureControl : MonoBehaviour
 
     void Update()
     {
-		totalForce = Vector3.zero;
-
-		if (obstacles.Count > 0)
+		if (neighbouringCreatures.Count == 0)
 		{
-			Vector3 obstacleRepulsionForce = getRepulsion(obstacles, obstacleTooClose);
-			obstacleRepulsionForce *= obstacleRepulsionMagnitude;
-
-			totalForce += obstacleRepulsionForce;
-
-			Debug.DrawRay(transform.position, obstacleRepulsionForce, Color.yellow);
+            totalForce = Vector3.zero;
+			return;
 		}
 
-		if (neighbouringCreatures.Count > 0)
-		{
-			Vector3 creatureRepulsionForce = getRepulsion(neighbouringCreatures, creatureTooClose);
-			creatureRepulsionForce *= creatureRepulsionMagnitude;
+		Vector3 repulsionForce = getRepulsion();
+		repulsionForce *= repulsionMagnitude;
 
-			Vector3 averagePosition;
-			Vector2 averageDirection;
-			getAverages(out averagePosition, out averageDirection);
+		Vector3 averagePosition;
+		Vector2 averageDirection;
+		getAverages(out averagePosition, out averageDirection);
 
-			Vector3 towardCenter = averagePosition - transform.position;
-			towardCenter.Normalize();
+		Vector3 towardCenter = averagePosition - transform.position;
+		towardCenter.Normalize();
 
-			Vector3 towardCenterForce = towardCenter * cohesionMagnitude;
+		Vector3 towardCenterForce = towardCenter * cohesionMagnitude;
 
-			Vector3 alignmentForce = (Vector3)averageDirection * alignmentMagnitude;
+		Vector3 alignmentForce = (Vector3)averageDirection * alignmentMagnitude;
 
-			totalForce += creatureRepulsionForce + towardCenterForce + alignmentForce;
-
-			Debug.DrawRay(transform.position, creatureRepulsionForce, Color.red);
-			Debug.DrawRay(transform.position, towardCenterForce, Color.green);
-			Debug.DrawRay(transform.position, alignmentForce, Color.blue);
-		}
-
+		totalForce = repulsionForce + towardCenterForce + alignmentForce;
 		float magnitude = Mathf.Min(maxMagnitude, totalForce.magnitude);
-		totalForce.Normalize();
-		totalForce *= magnitude;
+		totalForce = totalForce.normalized * magnitude;
 
+		Debug.DrawRay(transform.position, repulsionForce, Color.red);
+		Debug.DrawRay(transform.position, towardCenterForce, Color.green);
+		Debug.DrawRay(transform.position, alignmentForce, Color.blue);
 		Debug.DrawRay(transform.position, totalForce);
     }
 
@@ -92,13 +76,13 @@ public class CreatureControl : MonoBehaviour
 		averageDirection /= neighbouringCreatures.Count;
 	}
 
-	Vector3 getRepulsion(List<Transform> repulsors, float tooClose)
+	Vector3 getRepulsion()
 	{
 		float sqrTooClose = Mathf.Pow(tooClose, 2.0f);
 
 		Vector3 sumRepulsion = new Vector3();
 
-		foreach (var repulsor in repulsors)
+		foreach (var repulsor in neighbouringCreatures)
 		{
 			Vector3 awayVector = transform.position - repulsor.position;
 
@@ -119,10 +103,6 @@ public class CreatureControl : MonoBehaviour
         {
 			neighbouringCreatures.Add(creature.transform);
 		}
-		else
-		{
-			obstacles.Add(collider.transform);
-		}
     }
 
 	void OnTriggerExit2D(Collider2D collider)
@@ -131,10 +111,6 @@ public class CreatureControl : MonoBehaviour
 		if (creature != null)
 		{
 			neighbouringCreatures.Remove(creature.transform);
-		}
-		else
-		{
-			obstacles.Remove(collider.transform);
 		}
 	}
 }

dc3e91b5457ae8f9ae2e03734acbd6b0b830ac81
David Briscoe <idbrii@gmail.com>
2015-02-27 22:05:41 -0800

    Make spawning boundaries optional
    Only spawn boundaries if we have a boundary to spawn.

--- Assets/Scripts/GroundSpawner.cs
+++ Assets/Scripts/GroundSpawner.cs
@@ -16,26 +16,28 @@ public class GroundSpawner : Spawner {
         delta -= spriteOverlap;
         base.SpawnObjects(delta);
 
-        var total_bounds = new Bounds(transform.position, transform.position);
-        var renderers = GetComponentsInChildren(typeof(Renderer));
-        foreach (var component in renderers) {
-            var render = (Renderer)component;
-            total_bounds.Encapsulate(render.bounds);
+        if (boundary != null) {
+            var total_bounds = new Bounds(transform.position, transform.position);
+            var renderers = GetComponentsInChildren(typeof(Renderer));
+            foreach (var component in renderers) {
+                var render = (Renderer)component;
+                total_bounds.Encapsulate(render.bounds);
+            }
+
+            var size = new Vector2(delta.x, total_bounds.size.y);
+            var pos = total_bounds.min;
+            pos.y += total_bounds.size.y / 2.0f;
+            _SpawnBoundary(pos, size);
+            pos.x += total_bounds.size.x;
+            _SpawnBoundary(pos, size);
+
+            size = new Vector2(total_bounds.size.x, delta.y);
+            pos = total_bounds.min;
+            pos.x += total_bounds.size.x / 2.0f;
+            _SpawnBoundary(pos, size);
+            pos.y += total_bounds.size.y;
+            _SpawnBoundary(pos, size);
         }
-
-        var size = new Vector2(delta.x, total_bounds.size.y);
-        var pos = total_bounds.min;
-        pos.y += total_bounds.size.y / 2.0f;
-        _SpawnBoundary(pos, size);
-        pos.x += total_bounds.size.x;
-        _SpawnBoundary(pos, size);
-
-        size = new Vector2(total_bounds.size.x, delta.y);
-        pos = total_bounds.min;
-        pos.x += total_bounds.size.x / 2.0f;
-        _SpawnBoundary(pos, size);
-        pos.y += total_bounds.size.y;
-        _SpawnBoundary(pos, size);
     }
 
     Transform _SpawnBoundary(Vector3 pos, Vector2 size) {

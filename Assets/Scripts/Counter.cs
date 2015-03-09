53425ba69058df8e0e09a113511474e609468f69
David Briscoe <idbrii@gmail.com>
2015-03-08 17:24:17 -0700

    Add a counter to Enclosures
    Create a counter script and add it to the Enclosure prefab. It counts
    the number of objects that are enclosed and displays it via UI.

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Counter : MonoBehaviour {
+
+    // public so we can see the count when debugging.
+    [Tooltip("DO NOT EDIT: The number of items inside.")]
+    public int count = 0;
+
+    public int ContainedCount { get { return count; } }
+
+    Text counterDisplay;
+
+    void Start()
+    {
+        Dbg.Assert(count == 0, "You cannot edit Count.");
+
+        // The counter must have an output.
+        counterDisplay = GetComponentsInChildren<Text>()[0];
+    }
+
+    void Update()
+    {
+        counterDisplay.text = ""+ count;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        count += 1;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        count -= 1;
+    }
+}
